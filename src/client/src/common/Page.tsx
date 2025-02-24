4e858bef81c96c3e0a0cea81ddc946c8343437d0
Eva Stoddard <e.d.stoddard345@gmail.com>
2025-02-24 16:45:25 -0500

    Fix indentation size

--- src/client/src/common/Page.tsx
+++ src/client/src/common/Page.tsx
@@ -7,36 +7,36 @@ import { useNavigate } from 'react-router';
 
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const Page = ({ children }: Props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <Box>
-            <AppBar position='static' color='transparent' sx={{px: 2}}>
-                <Stack direction={"row"} alignItems={"center"}>
-                    <Divider orientation='vertical' sx={{mx: 2}} component={"p"} />
+  return (
+    <Box>
+      <AppBar position='static' color='transparent' sx={{ px: 2 }}>
+        <Stack direction={"row"} alignItems={"center"}>
+          <Divider orientation='vertical' sx={{ mx: 2 }} component={"p"} />
 
-                    <Button sx={{ height: 60 }} onClick={() => navigate('/')}>Events</Button>
+          <Button sx={{ height: 60 }} onClick={() => navigate('/')}>Events</Button>
 
-                    <Button sx={{ height: 60 }} onClick={() => navigate('/settings')}>Settings</Button>
+          <Button sx={{ height: 60 }} onClick={() => navigate('/settings')}>Settings</Button>
 
-                    <Button sx={{ height: 60 }} onClick={() => navigate('/account')}>Account</Button>
-                </Stack>
+          <Button sx={{ height: 60 }} onClick={() => navigate('/account')}>Account</Button>
+        </Stack>
 
-            </AppBar>
+      </AppBar>
 
-            <Box mx={5} mb={10}>
-                {children}
-            </Box>
+      <Box mx={5} mb={10}>
+        {children}
+      </Box>
 
-            <footer>
-                <Typography variant='body2'>
+      <footer>
+        <Typography variant='body2'>
 
-                </Typography>
-            </footer>
-        </Box>
-    )
+        </Typography>
+      </footer>
+    </Box>
+  )
 }
--- src/client/src/common/event/EventRow.tsx
+++ src/client/src/common/event/EventRow.tsx
@@ -3,22 +3,22 @@ import { EventItem } from "../../types/Event";
 import EditIcon from '@mui/icons-material/Edit';
 
 interface Props {
-    event: EventItem,
-    handleSelect: () => void,
+  event: EventItem,
+  handleSelect: () => void,
 }
 
 export function EventRow(props: Props) {
 
-    return (
-        <TableRow>
-            <TableCell>
-                <Stack direction={"column"} justifyContent={"center"}>
-                    <div>
-                        <a href={props.event.url}>{props.event.url}</a>
-                    </div>
-                    <Button startIcon={<EditIcon />}>Edit</Button>
-                </Stack>
-            </TableCell>
-        </TableRow>
-    )
+  return (
+    <TableRow>
+      <TableCell>
+        <Stack direction={"column"} justifyContent={"center"}>
+          <div>
+            <a href={props.event.url}>{props.event.url}</a>
+          </div>
+          <Button startIcon={<EditIcon />}>Edit</Button>
+        </Stack>
+      </TableCell>
+    </TableRow>
+  )
 }
