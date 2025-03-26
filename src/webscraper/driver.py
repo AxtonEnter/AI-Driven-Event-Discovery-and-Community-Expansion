39dbca929d808e56e83e3d9fb0100595e06fa593
Joe12827 <joewesn@gmail.com>
2025-03-26 15:31:32 -0400

    Added proxy class and implemented rotating proxy setup into driver

--- src/webscraper/driver.py
+++ src/webscraper/driver.py
@@ -1,12 +1,17 @@
 from playwright.sync_api import sync_playwright
 from playwright.async_api import async_playwright
+from proxy import OxylabsProxy
 import time
 import asyncio
 
 class PlaywrightDriver:
-    def __init__(self, headless: bool = True, proxy: bool = False):
-        """Initialize the Playwright driver with a browser instance."""
+    def __init__(self, headless: bool = True, proxy: OxylabsProxy = None):
+        """
+        Initialize the Playwright driver with a browser instance.
+        If given a list of ports, the driver will rotate IP addresses using a proxy.
+        """
         self.headless = headless
+        self.currentPort = None
         self.proxy = proxy
         self.playwright = None
         self.browser = None
@@ -20,21 +25,21 @@ class PlaywrightDriver:
         """Start the Playwright session asynchronously."""
         self.playwright = await async_playwright().start()
 
-        if self.proxy:
-            AUTH = "brd-customer-hl_c2040da7-zone-scraping_browser2:16iugatzsi52"
-            SBR_WS_CDP = f"wss://{AUTH}@brd.superproxy.io:9222"
-            self.browser = await self.playwright.chromium.connect_over_cdp(SBR_WS_CDP)
-        else:
-            self.browser = await self.playwright.chromium.launch(headless=self.headless)
+        self.browser = await self.playwright.chromium.launch(headless=self.headless)
 
         self.page = await self.browser.new_page()
 
         # Block images, media, and fonts
-        await self.page.route("**/*", lambda route, request: route.abort() if request.resource_type in ["image", "media", "font"] else route.continue_())
+        await self.page.route("**/*", lambda route, request: route.abort() if request.resource_type in ["image", "stylesheet", "font", "media"] else route.continue_())
 
         # Set up CDP session to capture network traffic
         self.context = self.page.context
         self.session = await self.context.new_cdp_session(self.page)
+
+        if self.proxy:
+            self.context = await self.browser.new_context(proxy={"server": self.proxy.getServer() + ":" + self.proxy.getCurrentPort(),
+                                                                 "username": self.proxy.getUsername(),
+                                                                 "password": self.proxy.getPassword()})
 
         # Track network responses
         async def log_traffic(event):
@@ -46,12 +51,24 @@ class PlaywrightDriver:
         self.session.on("Network.loadingFinished", log_traffic)
 
 
+    def rotateProxy(self):
+        """Rotate the proxy IP address."""
+        if self.proxy:
+            # Rotate the proxy IP address
+            self.proxy.nextPort()
+            self.context = self.browser.new_context(proxy={"server": self.proxy.getServer() + ":" + self.proxy.getCurrentPort(),
+                                                           "username": self.proxy.getUsername(),
+                                                           "password": self.proxy.getPassword()})
+            pass
 
     async def getHtml(self, url: str) -> str | None:
         """Fetch the HTML content of a webpage if it's an HTML page."""
         if not self.page:
             print("Error: Playwright not started. Call `await start()` first.")
             return None
+        
+        if self.proxy:
+            self.rotateProxy(self)
         
         self.currentBytes = 0
         
@@ -66,7 +83,10 @@ class PlaywrightDriver:
 
             # Navigate to the page
             responseBody = await self.page.goto(url, timeout=20000)
-            await self.page.wait_for_load_state('networkidle', timeout=20000)
+            try:
+                await self.page.wait_for_load_state("load", timeout=10000)  # Wait for load trigger
+            except:
+                await self.page.wait_for_load_state("networkidle", timeout=20000)  # If timeout, wait for network idle
 
             if not responseBody or responseBody.status != 200:
                 print(f"Failed to load the URL. Status code: {responseBody.status if responseBody else 'Unknown'}")
---
+++ src/webscraper/proxy.py
@@ -0,0 +1,36 @@
+
+
+
+
+class OxylabsProxy:
+    def __init__(self, username: str, password: str, ips: int):
+        self.server = "isp.oxylabs.io"
+        self.username = username
+        self.password = password
+        self.currentPort = 8001
+        self.maxPort = (8001 + ips - 1)
+
+        self.ipCheck = "https://ip.oxylabs.io/location"
+
+    def getServer(self):
+        return self.server
+    
+    def getUsername(self):
+        return self.username
+    
+    def getPassword(self):
+        return self.password
+
+    def getIPCheckURL(self):
+        return self.ipCheck
+
+    def getCurrentPort(self):
+        return self.currentPort
+
+    def nextPort(self):
+        if self.currentPort == self.maxPort:
+            self.currentPort = 8001
+        else:
+            self.currentPort += 1
+        return self.currentPort
+    
