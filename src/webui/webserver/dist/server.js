58fcc74a4b093930a1e21cbdbf2aef7c41b670b0
Eva Stoddard <e.d.stoddard345@gmail.com>
2025-06-28 11:41:58 -0400

    add tsx to root for run

--- src/webui/webserver/dist/server.js
+++ src/webui/webserver/dist/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
+import { createServer } from "http";
 import cors from "cors";
 import path from "path";
 import morgan from "morgan";
@@ -8,21 +9,14 @@ import process from "process";
 import compression from "compression";
 import { schema } from "./schema.js";
 import { setupDevAuth, setupSessions } from "./auth.js";
-import fs from "fs";
-import https from "https";
 import { expressMiddleware } from "@as-integrations/express5";
 const allowed_origins = [process.env.REACT_APP_ORIGIN, "https://studio.apollographql.com"];
 const CORS_CONFIG = {
     origin: process.env.REACT_APP_ORIGIN,
     credentials: true,
 };
-const credentials = {
-    key: fs.readFileSync("./cert/private-key.pem"),
-    cert: fs.readFileSync("./cert/certificate.pem"),
-};
 const __dirname = path.resolve(path.dirname(''));
 async function startServer() {
-    process.loadEnvFile(__dirname + "/.env");
     const app = express();
     app.use(cors());
     app.use(compression());
@@ -64,7 +58,7 @@ async function startServer() {
     app.use("/graphql", cors(CORS_CONFIG), express.json(), expressMiddleware(server, {
         context: async ({ req }) => ({ token: req.headers.token }),
     }));
-    const httpServer = https.createServer(credentials, app);
+    const httpServer = createServer(app);
     const PORT = process.env.PORT || 3000;
     console.log("dir: " + path.join(__dirname, '/client/dist'));
     httpServer.listen({ port: PORT }, () => console.log(`🚀 GraphQL-Server is running on https://localhost:${PORT}/graphql`));
