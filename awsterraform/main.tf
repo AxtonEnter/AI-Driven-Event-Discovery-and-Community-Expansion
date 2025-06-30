69a34e4b4438f62c56be74e1f5ce4b7e3507e635
724jth <61999220+724jth@users.noreply.github.com>
2025-06-30 17:55:13 -0400

    staged for network provisioning testing

--- awsterraform/main.tf
+++ awsterraform/main.tf
@@ -11,12 +11,13 @@ provider "aws" {
     region = "us-east-1"
 }
 
-module "queue" {
-  source = "./queue"
-}
+
 
 # we are handling variables in modules now each module will just call source for simplicity sake. 
 /*
+module "queue" {
+  source = "./queue"
+}
 module "model" {
   source        = "./model"
 }
