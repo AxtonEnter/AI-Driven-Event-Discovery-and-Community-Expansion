f01437ced83953906c3d096d70fc6e9c01ad48de
Eva Stoddard <e.d.stoddard345@gmail.com>
2025-07-23 14:57:18 -0400

    backup default route

--- src/webui/client/src/routes.tsx
+++ src/webui/client/src/routes.tsx
@@ -8,6 +8,7 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Events />} />
+        <Route path="/app" element={<Events />} />
         <Route path="/orgs" element={<Organizations />} />
         <Route path="/users" element={<UserManagement />} />
       </Routes>
