9b9e2c2240166a576ffcd6a1e9a39ef1ea29fd26
Eva Stoddard <e.d.stoddard345@gmail.com>
2025-08-03 19:28:39 -0400

    EventCsvUpload structure

--- src/webui/client/src/common/csv/EventCsvUpload.tsx
+++ src/webui/client/src/common/csv/EventCsvUpload.tsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Button, Stack } from "@mui/material";
 
 interface EventCsvUploadProps {
   onFileSelected?: (file: File) => void;
 }
 
 export const EventCsvUpload: React.FC<EventCsvUploadProps> = ({ onFileSelected }) => {
+  const [file, setFile] = useState<File | null>(null);
+  const inputFile = useRef<HTMLInputElement | null>(null);
+
+  const openFileDialog = () => {
+    inputFile.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      setFile(files[0]);
+      console.log("File selected:", file?.name);
+      if (onFileSelected) {
+        onFileSelected(files[0]);
+      }
+    } else {
+      setFile(null);
+    }
+  };
+
   return (
-    <div>
-      <label htmlFor="event-csv-upload">
-        Upload CSV (headers: orgId, url, rawText, images):
-      </label>
+    <Stack direction={"row"} justifyContent={"center"} pb={2} pt={2}>
       <input
-        id="event-csv-upload"
         type="file"
+        id="event-csv-upload"
+        ref={inputFile}
+        style={{ display: "none" }}
         accept=".csv"
-        onChange={e => {
-          const file = e.target.files?.[0];
-          if (file && onFileSelected) {
-            onFileSelected(file);
-          }
-        }}
+        onChange={handleFileChange}
       />
-    </div>
+      <Button color="info" variant="outlined" onClick={openFileDialog}>
+        Upload Event CSV (headers: orgId, url, rawText, images)
+      </Button>
+    </Stack>
   );
 };
