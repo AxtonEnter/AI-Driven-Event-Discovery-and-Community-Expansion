aea2ddd248b2b8cd8116bfef3edf4b435136cf6a
Eva Stoddard <e.d.stoddard345@gmail.com>
2025-08-03 14:16:04 -0400

    csv upload logging

--- src/webui/client/src/common/csv/CsvUpload.tsx
+++ src/webui/client/src/common/csv/CsvUpload.tsx
@@ -17,10 +17,15 @@ export function CsvUpload(props: CsvUploadProps) {
     }
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("handleFileChange", e);
         const files = e.target.files;
         if (files && files.length > 0) {
+            console.log("Files selected:", files);
             setFile(files[0]);
-            if (file) props.handleUpload(file);
+            if (file) {
+                console.log("File selected:", file.name);
+                props.handleUpload(file);
+            }
         }
         else {
             setFile(null);
